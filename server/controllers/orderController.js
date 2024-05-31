fe985dc6908794c096528ab94b62f94e44eb74fa
minhtrong456 <mtrong100@gmail.com>
2024-05-31 15:40:42 +0700

    idk

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -252,63 +252,69 @@ export const cancelOrder = async (req, res) => {
 
 export const vnPayPayment = async (req, res) => {
   try {
-    var ipAddr =
+    const ipAddr =
       req.headers["x-forwarded-for"] ||
       req.connection.remoteAddress ||
       req.socket.remoteAddress ||
-      req.connection.socket.remoteAddress;
+      (req.connection.socket ? req.connection.socket.remoteAddress : null);
 
-    var config = require("config");
-    var dateFormat = require("dateformat");
+    const tmnCode = process.env.VNPAY_TMN_CODE;
+    const secretKey = process.env.VNPAY_HASH_SECRET;
+    const vnpUrl = process.env.VNPAY_URL;
+    const returnUrl = process.env.VNPAY_RETURN_URL;
 
-    var tmnCode = config.get(`${process.env.VNPAY_TMN_CODE}`);
-    var secretKey = config.get(`${process.env.VNPAY_HASH_SECRET}`);
-    var vnpUrl = config.get(`${process.env.VNPAY_URL}`);
-    var returnUrl = config.get(`${process.env.VNPAY_RETURN_URL}`);
+    const date = new Date();
+    const createDate = dateFormat(date, "yyyymmddHHmmss");
+    const orderId = dateFormat(date, "HHmmss");
+    const amount = req.body.amount;
+    const bankCode = req.body.bankCode;
+    const orderInfo = req.body.orderDescription;
+    const orderType = req.body.orderType;
+    let locale = req.body.language;
 
-    var date = new Date();
-
-    var createDate = dateFormat(date, "yyyymmddHHmmss");
-    var orderId = dateFormat(date, "HHmmss");
-    var amount = req.body.amount;
-    var bankCode = req.body.bankCode;
-
-    var orderInfo = req.body.orderDescription;
-    var orderType = req.body.orderType;
-    var locale = req.body.language;
-    if (locale === null || locale === "") {
+    if (!locale) {
       locale = "vn";
     }
-    var currCode = "VND";
-    var vnp_Params = {};
-    vnp_Params["vnp_Version"] = "2.1.0";
-    vnp_Params["vnp_Command"] = "pay";
-    vnp_Params["vnp_TmnCode"] = tmnCode;
-    // vnp_Params['vnp_Merchant'] = ''
-    vnp_Params["vnp_Locale"] = locale;
-    vnp_Params["vnp_CurrCode"] = currCode;
-    vnp_Params["vnp_TxnRef"] = orderId;
-    vnp_Params["vnp_OrderInfo"] = orderInfo;
-    vnp_Params["vnp_OrderType"] = orderType;
-    vnp_Params["vnp_Amount"] = amount * 100;
-    vnp_Params["vnp_ReturnUrl"] = returnUrl;
-    vnp_Params["vnp_IpAddr"] = ipAddr;
-    vnp_Params["vnp_CreateDate"] = createDate;
-    if (bankCode !== null && bankCode !== "") {
+    const currCode = "VND";
+    let vnp_Params = {
+      vnp_Version: "2.1.0",
+      vnp_Command: "pay",
+      vnp_TmnCode: tmnCode,
+      vnp_Locale: locale,
+      vnp_CurrCode: currCode,
+      vnp_TxnRef: orderId,
+      vnp_OrderInfo: orderInfo,
+      vnp_OrderType: orderType,
+      vnp_Amount: amount * 100,
+      vnp_ReturnUrl: returnUrl,
+      vnp_IpAddr: ipAddr,
+      vnp_CreateDate: createDate,
+    };
+
+    if (bankCode) {
       vnp_Params["vnp_BankCode"] = bankCode;
     }
 
+    const sortObject = (obj) => {
+      const sorted = {};
+      const keys = Object.keys(obj).sort();
+      keys.forEach((key) => {
+        sorted[key] = obj[key];
+      });
+      return sorted;
+    };
+
     vnp_Params = sortObject(vnp_Params);
 
-    var querystring = require("qs");
-    var signData = querystring.stringify(vnp_Params, { encode: false });
-    var crypto = require("crypto");
-    var hmac = crypto.createHmac("sha512", secretKey);
-    var signed = hmac.update(new Buffer(signData, "utf-8")).digest("hex");
+    const signData = querystring.stringify(vnp_Params, { encode: false });
+    const hmac = crypto.createHmac("sha512", secretKey);
+    const signed = hmac.update(Buffer.from(signData, "utf-8")).digest("hex");
     vnp_Params["vnp_SecureHash"] = signed;
-    vnpUrl += "?" + querystring.stringify(vnp_Params, { encode: false });
 
-    res.redirect(vnpUrl);
+    const paymentUrl =
+      vnpUrl + "?" + querystring.stringify(vnp_Params, { encode: false });
+
+    res.redirect(paymentUrl);
   } catch (error) {
     console.error("Error in vnPayPayment controller:", error.message);
     res.status(500).json({ message: "Internal Server Error" });
