d9d3df88ff070b009c5491236f3c1e67f4c3de68
minhtrong456 <mtrong100@gmail.com>
2024-06-01 20:13:01 +0700

    fix

--- server/index.js
+++ server/index.js
@@ -10,12 +10,35 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-// CORS configuration
+// CORS configuration ver 1
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     if (!origin) return callback(null, true);
+//     callback(null, true);
+//   },
+//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//   credentials: true,
+//   allowedHeaders: [
+//     "Origin",
+//     "X-Requested-With",
+//     "Content-Type",
+//     "Accept",
+//     "Authorization",
+//   ],
+// };
+
+// CORS configuration ver 2
 const corsOptions = {
   origin: (origin, callback) => {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    callback(null, true);
+    if (
+      !origin ||
+      origin === `${process.env.LOCAL_CLIENT_URL}` ||
+      origin === `${process.env.LOCAL_CLIENT_URL_2}`
+    ) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
   },
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true,
