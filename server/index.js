8eca0e858d1e7831a97096b173d26365e4288146
minhtrong456 <mtrong100@gmail.com>
2024-06-01 19:43:20 +0700

    fix

--- server/index.js
+++ server/index.js
@@ -12,12 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: [
-      process.env.LOCAL_CLIENT_URL,
-      process.env.LOCAL_CLIENT_URL_2,
-      process.env.STRIPE_URL,
-      "https://js.stripe.com",
-    ],
+    origin: "*",
     credentials: true,
   })
 );
